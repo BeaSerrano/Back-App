baea7bd8276bddb3460159ad28871613bf4b7662
Bea Serrano <beaserranolucio@gmail.com>
2023-12-21 18:26:04 +0100

    mongo error

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Back-App
+# Back-App - Instalación
+
+1. tener node, si no >>> brew install node (para Mac)
+2. npm install
+3. instalar express:
+    3.1. npm init
+    3.2. entry: add.js
+    3.3. npm install express --save
+4. abrimos servidor en el puerto 3000 (dionde le hemos indicado): node app.js
+5. si no tenemos mongodb >>>  brew install mongodb (para Mac)
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express"),
     app = express(),
-    bodyParser = require("body-parser"),
-    methodOverride = require("method-override");
-mongoose = require("mongoose");
+    http = require("http"),
+    server = http.createServer(app),
+    mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -13,11 +13,16 @@ app.use(methodOverride());
 var router = express.Router();
 
 router.get("/", function (req, res) {
-    res.send("Hola mundo!");
+    res.send("Hello World!");
 });
 
 app.use(router);
 
-app.listen(3000, function () {
-    console.log("Node server running on http://localhost:3000");
+mongoose.connect("mongodb://localhost/tvshows", function (err, res) {
+    if (err) {
+        console.log("ERROR: connecting to Database. " + err);
+    }
+    app.listen(3000, function () {
+        console.log("Node server running on http://localhost:3000");
+    });
 });
